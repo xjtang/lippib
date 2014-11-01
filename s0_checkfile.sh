00f503e31957211054604e53e025520b71f89e51
Xiaojing Tang <xjtang@users.noreply.github.com>
2014-11-01 17:07:05 -0400

    Create File

---
+++ s0_checkfile.sh
@@ -0,0 +1,37 @@
+# Landsat Time Series Preprocessing 
+# Step 0
+# Check The Checksums of All File
+# One input argument sets the input folder
+# By Chris Holden
+# Modified by Xiaojing Tang
+
+# check if filder exist
+if [ ! -d $1 ]; then
+    echo "Error - $here is not a directory"
+fi
+# set working directory
+cd $1
+
+# loop over cksum files
+for checksum in $(find ./ -name 'L*.cksum'); do
+
+    # find basename of file and remove extension to match up with archive
+    bn=$(basename $checksum | awk -F '.' '{ print $1 }')
+    
+    # test to see if archive exists
+    archive=${bn}.tar.gz
+    if [ ! -f $archive ]; then
+        echo "$bn has no matching archive"
+        continue
+    fi
+    
+    # if archive exists, then validate checksum
+    test = $(cksum $archive)
+    if [ "$test" != "$(cat $checksum)" ]; then
+        echo "!!!!! WARNING $bn may be corrupted !!!!!"
+    else
+        echo "$bn is ok"
+    fi
+done
+
+echo "done!"
